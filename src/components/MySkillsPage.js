119c5f57548f6063e4a6f75cd123a32a379b9c30
Ariel <arielox.ag@gmail.com>
2022-03-19 00:34:03 -0300

    listo mySkylls

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,78 +1,90 @@
-import React from 'react';
+import { motion } from 'framer-motion';
 import styled, { ThemeProvider } from 'styled-components';
-import { lightTheme } from './Themes';
-import { Design, Develope } from './AllSvgs';
+import { lazy, Suspense } from 'react';
+import { lightTheme, mediaQueries } from './Themes';
 
-import LogoComponent from '../subComponents/LogoComponent';
-import SocialIcons from '../subComponents/SocialIcons';
-import PowerButton from '../subComponents/PowerButton';
-import ParticleComponent from '../subComponents/ParticleComponent';
-import BigTitle from '../subComponents/BigTitle';
+import { Design, Develope } from './AllSvgs';
+import Loading from '../subComponents/Loading';
+
+const SocialIcons = lazy(() => import('../subComponents/SocialIcons'));
+const PowerButton = lazy(() => import('../subComponents/PowerButton'));
+const LogoComponent = lazy(() => import('../subComponents/LogoComponent'));
+const ParticleComponent = lazy(() =>
+   import('../subComponents/ParticleComponent')
+);
+const BigTitle = lazy(() => import('../subComponents/BigTitle'));
 
 const MySkillsPage = () => {
    return (
       <ThemeProvider theme={lightTheme}>
-         <Box>
-            <LogoComponent theme="light" />
-            <SocialIcons theme="light" />
-            <PowerButton />
+         <Suspense fallback={<Loading />}>
+            <Box
+               key="skills"
+               initial={{ opacity: 0 }}
+               animate={{ opacity: 1, transition: { duration: 1 } }}
+               exit={{ opacity: 0, transition: { duration: 0.5 } }}
+            >
+               <LogoComponent theme="light" />
+               <SocialIcons theme="light" />
+               <PowerButton />
 
-            <ParticleComponent theme="light" />
+               <ParticleComponent theme="light" />
 
-            <Main>
-               <Title>
-                  <Design width={40} height={40} /> Designer
-               </Title>
+               <Main>
+                  <Title>
+                     <Design width={40} height={40} /> Designer
+                  </Title>
 
-               <Description>
-                  I love to create design which speaks, Keep it clean, minimal
-                  and simple.
-               </Description>
-               <Description>
-                  <strong>I like to Design</strong>
-                  <ul>
-                     <li>Web Design</li>
-                     <li>Mobile Apps</li>
-                  </ul>
-               </Description>
-               <Description>
-                  <strong>Tools</strong>
-                  <ul>
-                     <li>Figma</li>
-                  </ul>
-               </Description>
-            </Main>
+                  <Description>
+                     I love to create design which speaks, Keep it clean,
+                     minimal and simple.
+                  </Description>
+                  <Description>
+                     <strong>I like to Design</strong>
+                     <ul>
+                        <li>Web Design</li>
+                        <li>Mobile Apps</li>
+                     </ul>
+                  </Description>
+                  <Description>
+                     <strong>Tools</strong>
+                     <ul>
+                        <li>Figma</li>
+                     </ul>
+                  </Description>
+               </Main>
 
-            <Main>
-               <Title>
-                  <Develope width={40} height={40} /> Frontend Developer
-               </Title>
-               <Description>
-                  I value business or brand for which i'm creating, thus i enjoy
-                  bringing new ideas to life.
-               </Description>
-               <Description>
-                  <strong>Skills</strong>
-                  <p>
-                     Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind,
-                     Firebase etc.
-                  </p>
-               </Description>
-               <Description>
-                  <strong>Tools</strong>
-                  <p>VScode, Github, Codepen etc.</p>
-               </Description>
-            </Main>
+               <Main>
+                  <Title>
+                     <Develope width={40} height={40} /> Frontend Developer
+                  </Title>
+                  <Description>
+                     I value business or brand for which i'm creating, thus i
+                     enjoy bringing new ideas to life.
+                  </Description>
+                  <Description>
+                     <strong>Skills</strong>
+                     <p>
+                        Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind,
+                        Firebase etc.
+                     </p>
+                  </Description>
+                  <Description>
+                     <strong>Tools</strong>
+                     <p>VScode, Github, Codepen etc.</p>
+                  </Description>
+               </Main>
 
-            <BigTitle text="SKILLS" top="80%" right="30%" />
-         </Box>
+               <BigTitle text="SKILLS" top="80%" right="30%" />
+            </Box>
+         </Suspense>
       </ThemeProvider>
    );
 };
 
 export default MySkillsPage;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
    background-color: ${props => props.theme.body};
    width: 100vw;
    height: 100vh;
@@ -80,6 +92,18 @@ const Box = styled.div`
    display: flex;
    justify-content: space-evenly;
    align-items: center;
+
+   ${mediaQueries(50)`
+   flex-direction:column;  
+   padding:8rem 0;
+   height:auto;
+   
+   &>*:nth-child(5){
+      margin-bottom:5rem;}`};
+
+   ${mediaQueries(30)`
+   &>*:nth-child(5){
+      margin-bottom:4rem;}`};
 `;
 
 const Main = styled.div`
@@ -91,6 +115,14 @@ const Main = styled.div`
    height: 60vh;
    z-index: 3;
    line-height: 1.5;
+
+   ${mediaQueries(60)`
+   height: 55vh;`};
+
+   ${mediaQueries(50)`
+   width: 50vw;
+   height: max-content;`};
+
    cursor: pointer;
 
    font-family: 'Ubuntu Mono', monospace;
@@ -109,6 +141,22 @@ const Title = styled.h2`
    justify-content: center;
    align-items: center;
    font-size: calc(1em + 1vw);
+
+   ${mediaQueries(60)`
+   font-size:calc(0.8em + 1vw);`};
+
+   ${mediaQueries(50)`
+   font-size:calc(1em + 2vw);
+   margin-bottom:1rem;`};
+
+   ${mediaQueries(30)`
+   font-size:calc(1em + 1vw);`};
+
+   ${mediaQueries(25)`
+   font-size:calc(0.8em + 1vw);
+   svg{
+      width:30px;
+      height:30px;}`};
 
    ${Main}:hover & {
       & > * {
@@ -129,6 +177,15 @@ const Description = styled.div`
    ${Main}:hover & {
       color: ${props => props.theme.body};
    }
+
+   ${mediaQueries(50)`
+   font-size: calc(0.8em + 1vw);`};
+
+   ${mediaQueries(30)`
+   font-size:calc(0.7em + 1vw);`};
+
+   ${mediaQueries(25)`
+   font-size:calc(0.5em + 1vw);`};
 
    strong {
       margin-bottom: 1rem;
