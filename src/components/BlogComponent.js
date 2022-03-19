43867bce7a3360896569d8a1251f0a2c693c410f
Ariel <arielox.ag@gmail.com>
2022-03-18 23:57:17 -0300

    listo media query en blog

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -1,20 +1,12 @@
 import { motion } from 'framer-motion';
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { mediaQueries } from './Themes';
 
 // Framer motion configuration
 const Item = {
-   hidden: {
-      scale: 0,
-   },
-   show: {
-      scale: 1,
-      transition: {
-         type: 'spring',
-         duration: 0.5,
-      },
-   },
+   hidden: { scale: 0 },
+   show: { scale: 1, transition: { type: 'spring', duration: 0.5 } },
 };
 
 const BlogComponent = props => {
@@ -44,25 +36,39 @@ export default BlogComponent;
 const Container = styled(motion.div)``;
 
 const Box = styled(motion(NavLink))`
-   text-decoration: none;
-   height: 20rem;
-   width: calc(10rem + 15vw);
-   padding: 1rem;
-   color: ${props => props.theme.text};
-   border: 2px solid ${props => props.theme.text};
    backdrop-filter: blur(2px);
    box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
-   cursor: pointer;
+   text-decoration: none;
+   width: calc(10rem + 15vw);
+   height: 20rem;
+   border: 2px solid ${props => props.theme.text};
+   padding: 1rem;
+   color: ${props => props.theme.text};
 
    display: flex;
    flex-direction: column;
    z-index: 5;
 
+   cursor: pointer;
    &:hover {
       color: ${props => props.theme.body};
       background-color: ${props => props.theme.text};
       transition: all 0.3s ease;
    }
+
+   ${mediaQueries(50)`
+   width:calc(60vw);
+   `};
+
+   ${mediaQueries(30)`
+   height:18rem;
+   `};
+
+   ${mediaQueries(25)`
+   height:14rem;
+   padding:0.8rem;
+   backdrop-filter: none;
+   `};
 `;
 
 // como pone la imagen
@@ -77,6 +83,9 @@ const Image = styled.div`
    ${Box}:hover & {
       border: 1px solid ${props => props.theme.body};
    }
+
+   ${mediaQueries(25)`
+   height:70%;`};
 `;
 
 const Title = styled.h3`
@@ -87,16 +96,26 @@ const Title = styled.h3`
    font-weight: 700;
    border-bottom: 1px solid ${props => props.theme.text};
 
+   ${mediaQueries(40)`
+   font-size:calc(0.8em + 1vw);`};
+
+   ${mediaQueries(25)`
+   font-size:calc(0.6em + 1vw);`};
+
    ${Box}:hover & {
       border-bottom: 1px solid ${props => props.theme.body};
    }
 `;
 const HashTags = styled.div`
    padding: 0.5rem 0;
+   ${mediaQueries(25)`
+   font-size:calc(0.5em + 1vw);`};
 `;
 const Tag = styled.span`
    padding-right: 0.5rem;
 `;
 const Date = styled.span`
    padding: 0.5rem 0;
+   ${mediaQueries(25)`
+   font-size:calc(0.5em + 1vw);`};
 `;
